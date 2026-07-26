921d8ae9de40db31b10b16845cb837f85e2fde2b
stephkamga <ste.kamga@gmail.com>
2026-07-26 14:35:01 +0200

    Expose debug endpoint, exempt from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|manifest.json|icon-.*\\.png).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.json|icon-.*\\.png|api/debug).*)",
   ],
 };
